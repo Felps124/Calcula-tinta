5b0c3cb3fb900e818dff0017d59a18fbc0d04d65
Felps124 <88803004+Felps124@users.noreply.github.com>
2021-11-18 14:43:38 -0300

    subindo código do projeto

---
+++ main.py
@@ -0,0 +1,5 @@
+largura = int(input("Insira a largura da casa:"))
+altura = int(input("Insira a altura da casa:"))
+area = (largura * altura) / 2
+
+print("a qantidade de tinta a ser usada pr metro quadrado é {}". format(area))
